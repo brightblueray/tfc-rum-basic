16b37b8a6b6c2ca3616f0c4fbd1970e717ba94e6
brightblueray <rryjewski@hashicorp.com>
2023-06-24 13:38:19 -0400

    Multi-threaded

--- tfc_rum_count.py
+++ tfc_rum_count.py
@@ -6,7 +6,6 @@ import getpass
 import json
 import logging
 import os
-import pprint
 import requests
 import time
 from urllib.parse import urlparse
@@ -25,7 +24,7 @@ def tfapi_get (url,headers,params=None):
         except requests.exceptions.HTTPError as err:
             if response.status_code == 401:
                 logging.error("Authorization Error: 401 Unauthorized: {response.url}")
-                # break #Actually not fatal ;-)
+                break 
             elif response.status_code == 404:
                 logging.error(f"Forbidden Error: 404 Not Found: {response.url}")
                 break #Fatal
@@ -57,7 +56,9 @@ def tfapi_get_data (url, headers, params):
     return data
 
 
-
+##
+## tfapi_get_state: Gets the state file resources
+##
 def tfapi_get_state (url, headers, params):
     resources = []
     result = tfapi_get(url, headers, params)
@@ -66,6 +67,10 @@ def tfapi_get_state (url, headers, params):
     return resources
 
 
+
+##
+## get_resources: Helper Function that calls the resource API or state file API to capture resources
+##
 def get_resources(ws, base_url, api_ver, headers, params):
     rum = 0
     null_rs = 0
@@ -119,7 +124,7 @@ def parse_arguments():
         "-l",
         "--log-level",
         choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
-        default="ERROR",
+        default="WARNING",
         help="Set the logging level (default: ERROR)",
     )
     parser.add_argument(
@@ -143,15 +148,18 @@ def parse_arguments():
     parser.add_argument(
         '-f',
         '--file',
-        help="Output file for results"
+        help="Output file for results (COMING SOON)"
     )
     parser.add_argument(
         '--csv',
-        help="Output in CSV format"
+        help="Output in CSV format (COMING SOON)"
     )
     return parser.parse_args()
 
 
+##
+## print_summary: Prints the summary report 
+##
 def print_summary(rum_sum):
 
     # Initialize variables for subtotal and grand total
@@ -198,6 +206,11 @@ def print_summary(rum_sum):
     print(f"\nGrand Total:")
     print(row_format.format('', '', '', '', '', grand_total['rum'], grand_total['data_rs'], grand_total['null_rs'], grand_total['total']))
 
+
+
+##
+## process_oss: User specified a path for statefiles
+##
 def process_oss(args):
     logging.info(f"Processing OSS")
     rum_sum = []  # rum_sum a list of organization results
@@ -253,7 +266,9 @@ def process_oss(args):
         rum_sum.append(org_sum)
     return rum_sum
 
-
+# 
+# process_enterprise: subroutine for TFC/TFE
+#
 def process_enterprise(args):
     logging.info(f"Processing Enterprise")
     rum_sum = []  # rum_sum a list of organization results
@@ -293,36 +308,61 @@ def process_enterprise(args):
 
     # Iterate over each org
     for o in org_response:
-        logging.info(f"Processing Org: {o['id']}")
+        start_time = time.perf_counter()
         org_sum = {}  # Initialize org summary
         org_sum['id'] = o['id'] # Set the id to org_id being processed
         org_sum['workspaces'] = [] # Initialize the list of workspaces for the org
 
         ws_url = f"{base_url}{api_ver}/organizations/{o['id']}/workspaces" # build the url for ws list
         workspaces = tfapi_get_data(ws_url, headers, params) # Get all the workspaces for the org
-        
-        for ws in workspaces:
-            logging.info(f"Processing ws: {ws['id']}")
-            ws_sum = {}
-            ws_sum['id'] = ws['id']
-            ws_sum['name'] = ws['attributes']['name']
-            ws_sum['resource-count'] = ws['attributes']['resource-count']
-            ws_sum['terraform-version'] = ws['attributes']['terraform-version']
-            ws_sum['last-updated'] = ws['attributes']['latest-change-at']
-            rs_sum = {'rum':0 , 'null_rs':0, 'data_rs':0, 'total': 0}
-            if ws_sum['resource-count'] > 0:
-                rs_sum = get_resources(ws, base_url, api_ver, headers, params)   
-            ws_sum['resources'] = rs_sum
-                
-            org_sum['workspaces'].append(ws_sum)
-            
+
+        # # Single Threaded
+        # for ws in workspaces:
+        #     ws_sum = process_workspace(ws, base_url, api_ver, headers, params)    
+        #     org_sum['workspaces'].append(ws_sum)
+
+        # Multi-Threaded
+        with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
+            args_list = [(ws, base_url, api_ver, headers, params) for ws in workspaces]
+            workspace_results = executor.map(lambda args: process_workspace(*args), args_list)
+
+            for ws_sum in workspace_results:
+                org_sum['workspaces'].append(ws_sum)
+        # Multi-Threaded
+
+        # Append the org summary to RUM summary 
         rum_sum.append(org_sum)
+        elapsed_time = time.perf_counter() - start_time
+        print(f"Processed Org: {o['id']} in {elapsed_time:.3f} seconds")
+
+
     return rum_sum
+
+
+
+#
+# process_workspace: Helper fxn, allowing multiple workspaces to be processed in parallel
+# 
+def process_workspace(ws, base_url, api_ver, headers, params):
+    logging.info(f"Processing ws: {ws['id']}")
+    ws_sum = {}
+    ws_sum['id'] = ws['id']
+    ws_sum['name'] = ws['attributes']['name']
+    ws_sum['resource-count'] = ws['attributes']['resource-count']
+    ws_sum['terraform-version'] = ws['attributes']['terraform-version']
+    ws_sum['last-updated'] = ws['attributes']['latest-change-at']
+    rs_sum = {'rum':0 , 'null_rs':0, 'data_rs':0, 'total': 0}
+    if ws_sum['resource-count'] > 0:
+        rs_sum = get_resources(ws, base_url, api_ver, headers, params)   
+    ws_sum['resources'] = rs_sum
+    return ws_sum
+
 
 
 ##########################################
 ## MAIN
 ##########################################
+start_time = time.perf_counter()
 
 # Parse command line arguments
 args = parse_arguments()
@@ -336,3 +376,7 @@ else:
     rum_sum = process_enterprise(args)
 
 print_summary(rum_sum)
+
+end_time = time.perf_counter()
+elapsed_time = end_time - start_time
+print(f"Elapsed time: {elapsed_time:.3f} seconds")
